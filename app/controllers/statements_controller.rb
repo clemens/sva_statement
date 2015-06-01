c0a055e82d4cbf3615b05a891aa0d36cc427f87c
Clemens Kofler <clemens@railway.at>
2015-06-01 15:31:20 +0200

    Skip forgery protection

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -1,4 +1,6 @@
 class StatementsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def parse
     if params[:file].try(:content_type) == "application/pdf"
       statement = Statement.from_file(params[:file].path)
